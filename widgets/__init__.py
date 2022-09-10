71d3234acae8cfb6296e23895378791fda8e34a8
Taylor <tbwrightartist@gmail.com>
2022-09-10 13:34:33 -0500

    Forgot this one.

---
+++ widgets/__init__.py
@@ -0,0 +1,4 @@
+"""
+Everything in this directory should use purely PySide 2 classes. NOTHING from outside PySide2 or the standard library
+should be used. These are general purpose widgets for agnostic use in all DCCs and apps.
+"""
