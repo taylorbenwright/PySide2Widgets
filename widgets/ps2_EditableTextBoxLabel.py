45ab83ddbe4260fee9fdc09d75854909aa605fc3
Taylor <tbwrightartist@gmail.com>
2022-09-26 20:53:38 -0500

    Exposed layout

--- widgets/ps2_EditableTextBoxLabel.py
+++ widgets/ps2_EditableTextBoxLabel.py
@@ -12,18 +12,18 @@ class EditableTextBoxLabel(QtWidgets.QWidget):
     def __init__(self, label_text: str, *args, left_label=True, **kwargs):
         super(EditableTextBoxLabel, self).__init__(*args, **kwargs)
 
-        main_layout = QtWidgets.QHBoxLayout()
+        self.main_layout = QtWidgets.QHBoxLayout()
         label_widget = QtWidgets.QLabel(label_text)
         self.editable_text_widget = QtWidgets.QLineEdit()
         self.editable_text_widget.returnPressed.connect(self.return_pressed)
 
         if left_label:
-            main_layout.addWidget(label_widget)
-        main_layout.addWidget(self.editable_text_widget)
+            self.main_layout.addWidget(label_widget)
+        self.main_layout.addWidget(self.editable_text_widget)
         if not left_label:
-            main_layout.addWidget(label_widget)
+            self.main_layout.addWidget(label_widget)
 
-        self.setLayout(main_layout)
+        self.setLayout(self.main_layout)
 
     @property
     def line_text(self):
@@ -32,6 +32,11 @@ class EditableTextBoxLabel(QtWidgets.QWidget):
     @line_text.setter
     def line_text(self, value: str):
         self.editable_text_widget.setText(value)
+
+    @property
+    def layout(self):
+        return self.main_layout
+
 
     def return_pressed(self, *args):
         self.returnPressed.emit(self.editable_text_widget)
